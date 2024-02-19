f405400581603cb44664149291b9e5c7d2124d0b
Chyisiang <chyisiang8@gmail.com>
2024-02-19 13:52:45 +0800

    refactor status styling

--- components/Tickets.tsx
+++ components/Tickets.tsx
@@ -7,50 +7,52 @@ type TicketItemProps = {
 };
 
 async function Tickets({ query }: TicketItemProps) {
-  let statusStyle = "";
   const tickets = await getFilteredTickets(query);
-
-  // switch (status) {
-  //   case "OPEN":
-  //     statusStyle = "bg-red-500";
-  //     break;
-  //   case "IN_PROGRESS":
-  //     statusStyle = "bg-yellow-500";
-  //     break;
-  //   case "RESOLVED":
-  //     statusStyle = "bg-green-500";
-  //     break;
-  //   case "CLOSED":
-  //     statusStyle = "bg-gray-500";
-  //     break;
-  //   default:
-  //     statusStyle = "bg-white";
-  // }
 
   return (
     <>
-      <div className={`px-4 flex flex-col gap-4 ${statusStyle}`}>
-        {tickets.map((ticket) => (
-          <div
-            className="px-4 py-2 border border-solid border-slate-900 flex justify-between"
-            key={ticket.id}
-          >
-            <div className="flex-col">
-              <p>Issue: {ticket.title}</p>
-              <p>Description: {ticket.description}</p>
-              <p>{ticket.status}</p>
+      <div className="px-4 flex flex-col gap-4">
+        {tickets.map((ticket) => {
+          let statusStyle = "";
+          switch (ticket.status) {
+            case "OPEN":
+              statusStyle = "bg-red-500";
+              break;
+            case "IN_PROGRESS":
+              statusStyle = "bg-yellow-500";
+              break;
+            case "RESOLVED":
+              statusStyle = "bg-green-500";
+              break;
+            case "CLOSED":
+              statusStyle = "bg-gray-500";
+              break;
+            default:
+              statusStyle = "bg-white";
+          }
+
+          return (
+            <div
+              className={`px-4 py-2 border border-solid border-slate-900 flex justify-between ${statusStyle}`}
+              key={ticket.id}
+            >
+              <div className="flex-col">
+                <p>Issue: {ticket.title}</p>
+                <p>Description: {ticket.description}</p>
+                <p>{ticket.status}</p>
+              </div>
+              <div className="flex flex-row gap-2 items-center">
+                <Link
+                  className="rounded-md border px-4 py-2 hover:bg-blue-500"
+                  href={`/entry/${ticket.id}/edit`}
+                >
+                  <span>Edit</span>
+                </Link>
+                <DeleteTicket id={ticket.id} />
+              </div>
             </div>
-            <div className="flex flex-row gap-2 items-center">
-              <Link
-                className="rounded-md border px-4 py-2 hover:bg-blue-500"
-                href={`/entry/${ticket.id}/edit`}
-              >
-                <span>Edit</span>
-              </Link>
-              <DeleteTicket id={ticket.id} />
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
