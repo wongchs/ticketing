200b71fd9065e0c8d10f4aa9f86b0554cc4bd12d
Chyisiang <chyisiang8@gmail.com>
2024-02-19 09:57:05 +0800

    simple stylings

--- components/Tickets.tsx
+++ components/Tickets.tsx
@@ -9,9 +9,30 @@ type TicketItemProps = {
 };
 
 function Tickets({ id, title, description, status }: TicketItemProps) {
+  let statusStyle = "";
+
+  switch (status) {
+    case "OPEN":
+      statusStyle = "bg-red-500";
+      break;
+    case "IN_PROGRESS":
+      statusStyle = "bg-yellow-500";
+      break;
+    case "RESOLVED":
+      statusStyle = "bg-green-500";
+      break;
+    case "CLOSED":
+      statusStyle = "bg-gray-500";
+      break;
+    default:
+      statusStyle = "bg-white";
+  }
+
   return (
     <>
-      <div className="py-2 px-4 border border-solid border-slate-900">
+      <div
+        className={`py-2 px-4 border border-solid border-slate-900 ${statusStyle}`}
+      >
         <p>Issue: {title}</p>
         <p>Description: {description}</p>
         <p>{status}</p>
